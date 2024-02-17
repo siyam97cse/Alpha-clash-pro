4f25bbe92a59a4dab9d94e4de98dcb9fc46f8757
siyam97cse <bgsiyam97@gmail.com>
2024-02-17 14:47:31 +0600

    interface added

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,25 @@
+// function play(){
+//     console.log('play start now');
+//     //step-1: Hide the home screen. To hide the screen add the class hidden to the home section
+// const homeSection = document.getElementById('home-screen');
+// homeSection.classList.add('hidden');
+// // console.log(homeSection.classList)
+
+// // Show the playground
+// const playgroundSection = document.getElementById('play-ground')
+// playgroundSection.classList.remove('hidden');
+// // console.log(playgroundSection.classList);
+
+
+// }
+function continueGame(){
+//step-1; generate a random alphabet
+// 
+
+
+}
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
+}
---
+++ scripts/utility.js
@@ -0,0 +1,13 @@
+function hideElementById(elementId){
+  const element = document.getElementById(elementId);  
+  element.classList.add('hidden');
+}
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+function getARandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('';
+    console.log(alphabets);
+}
